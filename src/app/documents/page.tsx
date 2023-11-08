2195059b3df8a6e4e1595055bf0ce514ecb97585
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-11-08 15:22:59 +0100

    documents pages

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,3 +1,20 @@
-export default function Documents() {
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
+export default async function Documents() {
+  const session = await getServerSession(authOptions)
+  console.log(session)
+  return (
+    <div>
+      {session && JSON.stringify(session)}
+      <h1>Documents</h1>
+      {/* {session && */}
+      {/*   <p> */}
+      {/*     You are currently signed in as <strong>{session.user.email}</strong>. */}
+      {/*   </p>} */}
+      <p>
+        This is the documents page. You can access this page at{" "}
+        <code>/documents</code>.
+      </p>
+    </div>);
 }
