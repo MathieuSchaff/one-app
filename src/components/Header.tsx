91133b7e6d3e3f84584203ec402c84fbd223773c
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-11-09 17:06:23 +0100

    added drop down menu for changing theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import { NavigationMenuDemo } from "./NavBar";
 import { AuthComponent } from "./AuthComponent";
+import { ModeToggle } from "./mode-toggle";
 export default function Header() {
   return (
-    <header className="flex items-center justify-between py-4 px-8 bg-blue-600 bg-opacity-90 shadow-md mx-8 mt-4 rounded-lg">
+    <header className="flex items-center justify-between py-4 px-8 bg-background  shadow-md mx-8 mt-4 rounded-lg">
       <NavigationMenuDemo />
-      <AuthComponent />
+      <div className="flex gap-4 items-center">
+        <AuthComponent />
+        <ModeToggle />
+      </div>
     </header>
   )
 }
